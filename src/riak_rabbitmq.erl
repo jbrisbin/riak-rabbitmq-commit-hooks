80ad54127525afa658cd7b239c1ab3c96a228aad
Kyle Hanson <me@khanson.io>
2016-02-15 19:08:12 -0800

    Update for Riak 2.0
    Change the level where the Meta variables are searched.

--- src/riak_rabbitmq.erl
+++ src/riak_rabbitmq.erl
@@ -8,51 +8,57 @@
 -define(BUCKET_META, <<"AMQP-Meta">>).
 -define(DELETED, <<"X-Riak-Deleted">>).
 -define(META, <<"X-Riak-Meta">>).
--define(EXCHANGE, "X-Riak-Meta-Amqp-Exchange").
--define(ROUTING_KEY, "X-Riak-Meta-Amqp-Routing-Key").
--define(HOST, "X-Riak-Meta-Amqp-Host").
--define(PORT, "X-Riak-Meta-Amqp-Port").
--define(VHOST, "X-Riak-Meta-Amqp-Vhost").
--define(USER, "X-Riak-Meta-Amqp-User").
--define(PASSWORD, "X-Riak-Meta-Amqp-Password").
--define(SKIP, "X-Riak-Meta-Amqp-Ignore").
+-define(EXCHANGE, <<"X-Riak-Meta-Amqp-Exchange">>).
+-define(ROUTING_KEY, <<"X-Riak-Meta-Amqp-Routing-Key">>).
+-define(HOST, <<"X-Riak-Meta-Amqp-Host">>).
+-define(PORT, <<"X-Riak-Meta-Amqp-Port">>).
+-define(VHOST, <<"X-Riak-Meta-Amqp-Vhost">>).
+-define(USER, <<"X-Riak-Meta-Amqp-User">>).
+-define(PASSWORD, <<"X-Riak-Meta-Amqp-Password">>).
+-define(SKIP, <<"X-Riak-Meta-Amqp-Ignore">>).
 
 -define(CONTENT_TYPE, <<"content-type">>).
 
 postcommit_send_amqp(RObj) ->
+  
+  MetadataObj = riak_object:get_metadata(RObj),
+  Headers = key_find(?META, MetadataObj, none, []),
+  % io:format("Obj meta: ~p~n", [MetadataObj]),
+  % io:format("Obj headers: ~p~n", [Headers]),
+    
   {ok, C} = riak:local_client(),
   PerBucketMetaObj = case C:get(riak_object:bucket(RObj), ?BUCKET_META) of
     {ok, PBMO} -> PBMO;
     _ -> none
   end,
-  % lager:log(info, "bucket meta obj: ~p", [PerBucketMetaObj]),
-  PerBucketMeta = case PerBucketMetaObj of
+  
+  PerBucketHeaders = case PerBucketMetaObj of
     none -> none;
     _ -> 
       PBM = riak_object:get_metadata(PerBucketMetaObj),
       key_find(?META, PBM, none, [])
   end,
-  % io:format("bucket meta: ~p~n", [PerBucketMeta]),
-  Metadata = riak_object:get_metadata(RObj),
-  Exchange = key_find(?EXCHANGE, Metadata, PerBucketMeta, riak_object:bucket(RObj)),
+  % io:format("bucket meta: ~p~n", [PerBucketHeaders]),
+  
+  Exchange = find(?EXCHANGE, Headers, PerBucketHeaders, riak_object:bucket(RObj)),
   % io:format("exchange: ~s~n", [Exchange]),
-  RoutingKey = key_find(?ROUTING_KEY, Metadata, PerBucketMeta, riak_object:key(RObj)),
+  RoutingKey = find(?ROUTING_KEY, Headers, PerBucketHeaders, riak_object:key(RObj)),
   % io:format("routing key: ~s~n", [RoutingKey]),
-  ContentType = list_to_binary(key_find(?CONTENT_TYPE, Metadata, PerBucketMeta, "application/octet-stream")),
+  ContentType = list_to_binary(key_find(?CONTENT_TYPE, MetadataObj, PerBucketHeaders, "application/octet-stream")),
   Body = riak_object:get_value(RObj),
 
   % Publish a message
   Publish = #'basic.publish'{ exchange=Exchange, routing_key=RoutingKey },
-  Headers = key_find(?META, Metadata, none, []),
+  
   
   case find(?SKIP, Headers, [], "false") of
     "true" -> ok;
     _ ->
       AppHdrs = lists:foldl(fun ({HdrKey, HdrValue}, NewHdrs) ->
-                                <<_:12/binary, Key/binary>> = list_to_binary(HdrKey),
+                                <<_:12/binary, Key/binary>> = HdrKey,
                                 [{binary_to_list(Key), binary, HdrValue} | NewHdrs]
                             end, [], Headers),
-      ExtraHdrs = [case key_find(?DELETED, Metadata, none, "false") of
+      ExtraHdrs = [case key_find(?DELETED, MetadataObj, none, "false") of
                      "true" -> {"X-Riak-Deleted", binary, "true"};
                      "false" -> []
                    end],
@@ -60,20 +66,20 @@ postcommit_send_amqp(RObj) ->
 
       Msg = #amqp_msg{ payload=Body, props=Props },
       % io:format("message: ~p~n", [Msg]),
-      {ok, Channel} = amqp_channel(RObj, PerBucketMetaObj),
+      {ok, Channel} = amqp_channel(Headers, PerBucketHeaders),
       % io:format("channel: ~p~n", [Channel]),
       amqp_channel:cast(Channel, Publish, Msg),
       % io:format("published~n"),
       ok
   end.
 
-amqp_channel(RObj, PerBucketMetaObj) ->
-  AmqpParams = amqp_p(RObj, PerBucketMetaObj),
+amqp_channel(Headers, PerBucketMeta) ->
+  AmqpParams = amqp_p(Headers, PerBucketMeta),
   % io:format("amqp params: ~p~n", [AmqpParams]),
   case pg2:get_closest_pid(AmqpParams) of
     {error, {no_such_group, _}} -> 
       pg2:create(AmqpParams),
-      amqp_channel(RObj, PerBucketMetaObj);
+      amqp_channel(Headers, PerBucketMeta);
     {error, {no_process, _}} -> 
       % io:format("no client running~n"),
       case amqp_connection:start(AmqpParams) of
@@ -94,18 +100,16 @@ amqp_channel(RObj, PerBucketMetaObj) ->
       {ok, Channel}
   end.
 
-amqp_p(RObj, PerBucketMetaObj) ->
-  Metadata = riak_object:get_metadatas(RObj),
-  PerBucketMetadata = case PerBucketMetaObj of
-    none -> [];
-    _ -> riak_object:get_metadatas(PerBucketMetaObj)
-  end,
-
-  Host = find(?HOST, Metadata, PerBucketMetadata, <<"127.0.0.1">>),
-  Port = find(?PORT, Metadata, PerBucketMetadata, 5672),
-  Vhost = find(?VHOST, Metadata, PerBucketMetadata, <<"/">>),
-  User = find(?USER, Metadata, PerBucketMetadata, <<"guest">>),
-  Pass = find(?PASSWORD, Metadata, PerBucketMetadata, <<"guest">>),
+amqp_p(Headers, PerBucketHeaders) ->
+  
+  % io:format("ObjMeta: ~p~n", [Headers]),
+  % io:format("BucketMeta: ~p~n", [PerBucketHeaders]),
+  
+  Host = find(?HOST, Headers, PerBucketHeaders, <<"127.0.0.1">>),
+  Port = find(?PORT, Headers, PerBucketHeaders, 5672),
+  Vhost = find(?VHOST, Headers, PerBucketHeaders, <<"/">>),
+  User = find(?USER, Headers, PerBucketHeaders, <<"guest">>),
+  Pass = find(?PASSWORD, Headers, PerBucketHeaders, <<"guest">>),
 
   #amqp_params_network{username = User,
                        password = Pass,
@@ -113,29 +117,29 @@ amqp_p(RObj, PerBucketMetaObj) ->
                        host = binary_to_list(Host),
                        port = Port}.
 
-find(K, L, PerBucketMetadata, Default) ->
+find(K, L, PerBucketHeaders, Default) ->
   case lists:keyfind(K, 1, L) of
     {K, V} -> V;
     _ -> 
-      case PerBucketMetadata of
+      case PerBucketHeaders of
         none -> Default;
         _ ->
-          case lists:keyfind(K, 1, PerBucketMetadata) of
+          case lists:keyfind(K, 1, PerBucketHeaders) of
             {K, BucketV} -> BucketV;
             _ -> Default
           end
       end
   end.
   
-key_find(K, D, BucketMeta, Default) ->
+key_find(K, D, PerBucketHeaders, Default) ->
   case dict:find(K, D) of
     {ok, V} -> V;
     _ -> 
-      % io:format("per-bucket meta: ~p~n", [BucketMeta]),
-      case BucketMeta of
+      % io:format("per-bucket meta: ~p~n", [PerBucketHeaders]),
+      case PerBucketHeaders of
         none -> Default;
         _ -> 
-          case find(K, BucketMeta, none, Default) of
+          case find(K, PerBucketHeaders, none, Default) of
             PerBucketV when is_binary(PerBucketV) -> PerBucketV;
             PerBucketV -> list_to_binary(PerBucketV)
           end
